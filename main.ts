75d1831784786713ccc1e595c7306b9ec3ae0d79
Isaac Jessup <ibjessup@gmail.com>
2024-10-21 23:17:58 +0000

    no async

--- main.ts
+++ main.ts
@@ -3,8 +3,8 @@
 import { DuckDB } from "./src/index.ts";
 
 const duckDB = new DuckDB();
-await duckDB.open();
+duckDB.open();
 console.log("Database opened.");
-await duckDB.connect();
+duckDB.connect();
 console.log("Connected to the database.");
 console.log("Library Version:", duckDB.getLibraryVersion());
